606a2748438642d007036923328112ace2dcd724
Cairo Noleto <caironoleto@gmail.com>
2010-08-12 23:27:33 -0300

    Após salvar um texto, é colocado no delayed job

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < ActiveRecord::Base
   belongs_to :domain
+  after_save :add_process_in_delayed_job
+  
+  def add_process_in_delayed_job
+    process
+  end
+  
+  handle_asynchronously :add_process_in_delayed_job
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,12 +5,34 @@ describe Article do
     @valid_attributes = {
       :title => "value for title",
       :text => "value for text",
-      :origin_url => "value for origin_url",
+      :origin_url => "#{Rails.root}/spec/fixtures/180_page.html",
       :published_at => Time.now
     }
   end
+  
+  subject do
+    Article.new(@valid_attributes)
+  end
+  
+  before(:each) do
+    valid_attributes = {
+      :title => "value for title",
+      :url => "value for url",
+      :feed_url => "#{File.read("spec/fixtures/180_rss.rss")}",
+      :nokogiri_search_field => "texto_materia"
+    }
 
-  it "should create a new instance given valid attributes" do
-    Article.create!(@valid_attributes)
+    domain = Domain.create!(valid_attributes)
+    subject.update_attributes(:domain => domain)
+  end
+
+  it "should add in DJ after save" do
+    lambda {
+      subject.save 
+    }.should change(Delayed::Backend::ActiveRecord::Job, :count).by(1)
+  end
+  
+  it "should get new text" do
+    
   end
 end

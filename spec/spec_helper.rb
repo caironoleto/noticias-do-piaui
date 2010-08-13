7c52171c529fcf6237f3793650ceda7a3eaa6cdb
Cairo Noleto <caironoleto@gmail.com>
2010-08-12 23:28:25 -0300

    Apagando o delayed job antes e depois dos testes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,13 @@ Spec::Runner.configure do |config|
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
   config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+  config.before(:each) do
+    Delayed::Backend::ActiveRecord::Job.delete_all
+  end
+
+  config.after(:each) do
+    Delayed::Backend::ActiveRecord::Job.delete_all
+  end
 
   # == Fixtures
   #

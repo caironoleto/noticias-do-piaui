f6fde9e2a17ba3e05a6baf91579a9c88e0af0540
Cairo Noleto <caironoleto@gmail.com>
2010-08-12 22:31:13 -0300

    Adicionando feedzirra no env

--- config/environment.rb
+++ config/environment.rb
@@ -33,6 +33,7 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
+  config.gem "feedzirra"
   config.time_zone = 'UTC'
 
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.

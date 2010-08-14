2eb241b1218f916cd86565f19ee080b95cabc616
Cairo Noleto <caironoleto@gmail.com>
2010-08-14 01:05:29 -0300

    nomeando uma rota

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect "noticias/:page", :controller => :articles, :action => :index, :page => /\d+/
-  map.connect "noticias/:slug", :controller => :articles, :action => :show
+  map.article "noticias/:slug", :controller => :articles, :action => :show
   map.connect "noticias/", :controller => :articles, :action => :index
   map.root :controller => :articles, :action => :index
 end

074d8f42c25eaa81dc5ddf49248a4fa2b3030706
Cairo Noleto <caironoleto@gmail.com>
2010-08-13 23:07:49 -0300

    Processando o texto

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,6 +5,25 @@ class Article < ActiveRecord::Base
   def add_process_in_delayed_job
     process
   end
-  
+
   handle_asynchronously :add_process_in_delayed_job
+  
+  def process
+    self.text = ""
+    Nokogiri::HTML(open(origin_url)).search(domain.nokogiri_search_field).first.children.each do |element|
+      if element.content.present?
+        content = element.content
+        content.gsub!("\n", "<br />")
+        content.gsub!("\r", "")
+        content.gsub!("\t", " ")
+        content.lstrip!
+      end
+
+      if element.name == domain.paragraph_tag && content.present? && content.size > 8
+        self.text << "<p>#{content}</p>"
+      end
+    end
+    self.published_at = Time.now
+    save
+  end
 end

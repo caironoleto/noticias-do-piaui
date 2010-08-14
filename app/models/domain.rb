9a652bf0b9bceeeaece84e044e58e28fb85ebd88
Cairo Noleto <caironoleto@gmail.com>
2010-08-14 01:04:25 -0300

    Agora após cadastrar, processa o artigo

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,7 +2,10 @@ class Domain < ActiveRecord::Base
   has_many :articles
   def process
     Feedzirra::Feed.fetch_and_parse(feed_url).entries.each do |entry|
-      articles.create(:title => entry.title, :origin_url => entry.url) unless articles.find_by_origin_url(entry.url).present?
+      unless articles.find_by_origin_url(entry.url).present?
+        article = articles.create(:title => entry.title, :origin_url => entry.url)
+        article.process
+      end
     end
   end
 end

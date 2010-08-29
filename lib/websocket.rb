5cb58da89ee75992c3217e7f256361b21ac2dd8d
Cairo Noleto <caironoleto@gmail.com>
2010-08-29 16:34:49 -0300

    Adicionando servidor WebSocket

---
+++ lib/websocket.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+ENV["RAILS_ENV"] = "development"
+ENV["DOMAIN"] = "localhost"
+if ARGV.index('--')
+  ENV["RAILS_ENV"] = ARGV[ARGV.index('--') + 1]
+  if ENV["RAILS_ENV"] == "production"
+    ENV["DOMAIN"] = "http://news.caironoleto.com"
+  end
+end
+
+require "rubygems"
+require "lib/websocket/lib/web_socket"
+require File.join(File.dirname(__FILE__), '..', 'config', 'environment')
+require "simple-daemon"
+
+class Processor < SimpleDaemon::Base
+  SimpleDaemon::WORKING_DIRECTORY = "log" 
+
+  def self.start
+    loop do  
+      server = WebSocketServer.new(:accepted_domains => [ENV["DOMAIN"]], :port => 3001)
+      server.run() do |ws|
+        ws.handshake()
+        loop do
+          article = Article.first(:order => "published_at desc", :conditions => "text <> ''").to_json(:include => {:domain => {:only => [:url, :title]}})
+          ws.send(article)
+          sleep(5)
+        end
+      end
+    end
+  end
+
+  def self.stop
+    puts "Stopping processor  " 
+  end
+end
+
+Processor.daemonize

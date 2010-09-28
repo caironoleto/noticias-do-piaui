b32fbbb9dc7e85853ee00dc1feecbbddbab3839e
Cairo Noleto <caironoleto@gmail.com>
2010-09-28 00:21:53 -0300

    Remove tweet button helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,3 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  include TweetButton
-  TweetButton.default_tweet_button_options = {:via => "noticiasdopiaui", :count => "horizontal"}
 end

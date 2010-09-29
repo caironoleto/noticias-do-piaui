396052a1aac26cd553463a54bf0dfcd1e6225e21
Cairo Noleto <caironoleto@gmail.com>
2010-09-29 20:56:24 -0300

    Changed to ready named scope

--- app/controllers/domains_controller.rb
+++ app/controllers/domains_controller.rb
@@ -2,6 +2,6 @@ class DomainsController < ApplicationController
   def show
     @domain = Domain.find_by_slug(params[:slug])
     @title = @domain.title
-    @articles = @domain.articles.paginate(:per_page => 30, :page => params.fetch(:page, 1).to_i, :order => "published_at desc", :conditions => "text <> ''")
+    @articles = @domain.articles.ready.paginate(:per_page => 30, :page => params.fetch(:page, 1).to_i)
   end
 end
--- spec/controllers/domains_controller_spec.rb
+++ spec/controllers/domains_controller_spec.rb
@@ -12,35 +12,41 @@ describe DomainsController do
   describe "GET show" do
     it "should render show template" do
       Domain.stub!(:find_by_slug).and_return(mock_domain(:articles => [mock_article], :title => ""))
+      mock_domain.articles.stub!(:ready).and_return([mock_article])
       get :show, :slug => "180-graus"
     end
     
     it "should expose domain as @domain " do
       Domain.should_receive(:find_by_slug).with("meio-norte").and_return(mock_domain(:articles => [mock_article], :title => ""))
+      mock_domain.articles.stub!(:ready).and_return([mock_article])
       get :show, :slug => "meio-norte"
       assigns[:domain].should eql mock_domain
     end
     
-    
-    it "should expose domain title as @title" do
-      Domain.should_receive(:find_by_slug).with("meio-norte").and_return(mock_domain(:articles => [mock_article]))
-      mock_domain.should_receive(:title).and_return("This is my title")
-      get :show, :slug => "meio-norte"
-      assigns[:title].should eql "This is my title"
-    end
-    
     it "should expose article as @articles" do
-      Domain.should_receive(:find_by_slug).with("cidade-verde").and_return(mock_domain(:articles => [mock_article], :title => ""))
-      mock_domain.articles.should_receive(:paginate).with(:per_page => 30, :page => 1, :order => "published_at desc", :conditions => "text <> ''").and_return([mock_article])
+      articles = [mock_article]
+      Domain.should_receive(:find_by_slug).with("cidade-verde").and_return(mock_domain(:articles => articles, :title => ""))
+      mock_domain.articles.should_receive(:ready).and_return(articles)
+      mock_domain.articles.should_receive(:paginate).with(:per_page => 30, :page => 1).and_return(articles)
       get :show, :slug => "cidade-verde"
-      assigns[:articles].should eql [mock_article]
+      assigns[:articles].should eql articles
     end
     
     it "should expose article as @articles with different page" do
-      Domain.should_receive(:find_by_slug).with("cidade-verde").and_return(mock_domain(:articles => [mock_article], :title => ""))
-      mock_domain.articles.should_receive(:paginate).with(:per_page => 30, :page => 2, :order => "published_at desc", :conditions => "text <> ''").and_return([mock_article])
+      articles = [mock_article]
+      Domain.should_receive(:find_by_slug).with("cidade-verde").and_return(mock_domain(:articles => articles, :title => ""))
+      mock_domain.articles.should_receive(:ready).and_return(articles)
+      mock_domain.articles.should_receive(:paginate).with(:per_page => 30, :page => 2).and_return(articles)
       get :show, :slug => "cidade-verde", :page => "2"
       assigns[:articles].should eql [mock_article]
+    end
+    
+    it "should expose domain title as @title" do
+      Domain.should_receive(:find_by_slug).with("meio-norte").and_return(mock_domain(:articles => [mock_article]))
+      mock_domain.articles.stub!(:ready).and_return([mock_article])
+      mock_domain.should_receive(:title).and_return("This is my title")
+      get :show, :slug => "meio-norte"
+      assigns[:title].should eql "This is my title"
     end
   end
 end

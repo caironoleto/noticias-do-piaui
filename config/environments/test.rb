1025f990b2bc691b3b3f0827d3168d0ae9e39e96
Cairo Noleto <caironoleto@gmail.com>
2010-10-05 23:20:11 -0300

    Added database cleaner

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,3 +30,4 @@ config.action_mailer.delivery_method = :test
 config.gem 'rspec-rails', :version => '>= 1.3.2', :lib => false unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 config.gem 'steak'
 config.gem 'capybara'
+config.gem 'database_cleaner'

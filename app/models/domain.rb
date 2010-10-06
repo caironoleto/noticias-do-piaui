c76adb2433654eafd8f79089350667da741e54ad
Cairo Noleto <caironoleto@gmail.com>
2010-10-06 08:44:34 -0300

    Find by slug

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -17,7 +17,7 @@ class Domain < ActiveRecord::Base
       feed = Feedzirra::Feed.fetch_and_parse(feed_url)
       if feed.present? && feed.respond_to?(:entries)
         feed.entries.each do |entry|
-          unless articles.find_by_origin_url(entry.url.remove_www).present?
+          unless articles.find_by_slug(entry.title.parameterize.to_s).present?
             article = articles.create(:title => entry.title, :origin_url => entry.url)
             article.process
           end
@@ -32,7 +32,7 @@ class Domain < ActiveRecord::Base
       entries = Nokogiri::HTML(open(last_news_url)).search(last_news_search_field)
       if entries.present?
         entries.each do |entry|
-          unless articles.find_by_origin_url(entry.attributes["href"].content.remove_www).present?
+          unless articles.find_by_slug(entry.attributes["title"].content.parameterize.to_s).present?
             article = articles.create(:title => entry.attributes["title"].content, :origin_url => entry.attributes["href"].content)
             article.process
           end
